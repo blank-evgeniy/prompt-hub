996707075121798b5846bcf713fb3ad85edb3d03
Evgeniy Blank <blank.eugeny@yandex.ru>
2025-08-24 10:47:00 +0200

    setup linters, styles

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+
+import '@/app/styles/globals.css'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin', 'cyrillic'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin', 'cyrillic'],
+})
+
+export const metadata: Metadata = {
+  title: 'Prompt Hub',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="ru">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}

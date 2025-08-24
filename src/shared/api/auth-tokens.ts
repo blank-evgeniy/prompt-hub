9eb8048d882d1025e2230153bc73ba92972c95b0
Evgeniy Blank <blank.eugeny@yandex.ru>
2025-08-24 11:05:05 +0200

    feat(api): add axios instance

---
+++ src/shared/api/auth-tokens.ts
@@ -0,0 +1,15 @@
+import Cookies from 'js-cookie'
+import { ACCESS_TOKEN_KEY } from './consts'
+
+export const getAccessToken = () => {
+  const authToken = Cookies.get(ACCESS_TOKEN_KEY)
+  return authToken || null
+}
+
+export const saveAccessToken = (authToken: string) => {
+  Cookies.set(ACCESS_TOKEN_KEY, authToken, { expires: 7 })
+}
+
+export const clearAccessToken = () => {
+  Cookies.remove(ACCESS_TOKEN_KEY)
+}
---
+++ src/shared/api/index.ts
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import { getAccessToken } from './auth-tokens'
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+})
+
+axiosInstance.interceptors.request.use(
+  async (request) => {
+    const accessToken = getAccessToken()
+    if (accessToken) {
+      request.headers['Authorization'] = `Bearer ${accessToken}`
+    }
+    return request
+  },
+  (error) => Promise.reject(error)
+)

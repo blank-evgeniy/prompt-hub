92c9d2eecfbf05cb0de579e08b133fa5a1c3312e
Evgeniy Blank <blank.eugeny@yandex.ru>
2025-11-14 22:20:45 +0200

    fix: ssr prefetch

--- app/(main)/profile/prompts/page.tsx
+++ app/(main)/profile/prompts/page.tsx
@@ -1,20 +1,5 @@
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from '@tanstack/react-query'
-
 import { ProfilePromptsPage } from '@/pages/profile-prompts'
-import { promptQueries } from '@/shared/api/services/prompt'
 
 export default async function ListPrompts() {
-  const queryClient = new QueryClient()
-
-  await queryClient.prefetchQuery(promptQueries.profileList())
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <ProfilePromptsPage />
-    </HydrationBoundary>
-  )
+  return <ProfilePromptsPage />
 }
--- app/(main)/user/[username]/page.tsx
+++ app/(main)/user/[username]/page.tsx
@@ -1,25 +1,10 @@
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from '@tanstack/react-query'
-
 import { ProfilePage } from '@/pages/profile'
-import { usersQueries } from '@/shared/api/services'
 import { PageParams } from '@/shared/types'
 
 const User = async ({ params }: PageParams<{ username: string }>) => {
   const { username } = await params
 
-  const queryClient = new QueryClient()
-
-  await queryClient.prefetchQuery(usersQueries.one(username))
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <ProfilePage username={username} />
-    </HydrationBoundary>
-  )
+  return <ProfilePage username={username} />
 }
 
 export default User
